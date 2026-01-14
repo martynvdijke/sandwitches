5e007294390e9daf163b0a146e65121a08028ca2
Martyn van Dijke <martijnvdijke600@gmail.com>
2026-01-14 10:56:04 +0100

    feat: Add singleton settings model and webpack for static assets

---
+++ src/static/js/index.js
@@ -0,0 +1,8 @@
+// src/static/js/index.js
+import 'beercss/dist/cdn/beer.min.css';
+import 'beercss/dist/cdn/beer.min.js';
+import 'material-dynamic-colors/dist/cdn/material-dynamic-colors.min.js';
+import 'htmx.org/dist/htmx.min.js';
+
+// You can add your own custom JS here
+console.log('Webpack bundle loaded!');
