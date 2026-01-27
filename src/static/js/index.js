1e729615df399a626cf7361675bd7668633d395d
Martyn van Dijke <martijnvdijke600@gmail.com>
2026-01-27 16:32:40 +0100

    fix(web): serve dist and css ourselves

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -3,7 +3,12 @@ import 'htmx.org/dist/htmx.min.js';
 import 'beercss/dist/cdn/beer.min.css';
 import 'beercss/dist/cdn/beer.min.js';
 import Chart from 'chart.js/auto';
+import Cropper from 'cropperjs';
+import 'cropperjs/dist/cropper.css';
 
 // This makes Chart available to your HTML/scripts
-window.Chart = Chart;// You can add your own custom JS here
+window.Chart = Chart;
+window.Cropper = Cropper;
+
+// You can add your own custom JS here
 console.log('Webpack bundle loaded!');
